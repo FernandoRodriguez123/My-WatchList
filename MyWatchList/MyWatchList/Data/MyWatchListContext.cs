381dd549d459101edc917b09b0c18ff740939a18
Fernando <ferrodriteje@gmail.com>
2025-05-11 13:16:56 +0200

    DBContext

---
+++ MyWatchList/MyWatchList/Data/MyWatchListContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWatchList.Models;
+
+namespace MyWatchList.Data
+{
+    public class MyWatchListContext(string myDatabase) : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Serie> Series { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<UserSerieConfig> UserSerieConfigs { get; set; }
+        public DbSet<SerieGenre> SerieGenres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(myDatabase);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Clave compuesta para tabla de unión SerieGenre
+            modelBuilder.Entity<SerieGenre>()
+                .HasKey(sg => new { sg.SerieId, sg.GenreId });
+
+            //Relaciones entre Serie y SerieGenre
+            modelBuilder.Entity<SerieGenre>()
+                .HasOne(sg => sg.Serie)
+                .WithMany(s => s.SerieGenres)
+                .HasForeignKey(sg => sg.SerieId);
+
+            modelBuilder.Entity<SerieGenre>()
+                .HasOne(sg => sg.Genre)
+                .WithMany(g => g.SerieGenres)
+                .HasForeignKey(sg => sg.GenreId);
+
+            //Relaciones entre User y UserSerieConfig
+            modelBuilder.Entity<UserSerieConfig>()
+                .HasOne(usc => usc.User)
+                .WithMany(u => u.SeriesConfigs)
+                .HasForeignKey(usc => usc.UserId);
+
+            //Relaciones entre Serie y UserSerieConfig
+            modelBuilder.Entity<UserSerieConfig>()
+                .HasOne(usc => usc.Serie)
+                .WithMany(s => s.UserConfigs)
+                .HasForeignKey(usc => usc.SerieId);
+        }
+    }
+}
